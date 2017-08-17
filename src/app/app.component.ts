6e29c4bc931e0a06eb2b612d21ea7387c884ec18
fas84 <fernandoarozarena84@gmail.com>
2017-08-17 15:20:02 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit} from '@angular/core';
+import {Http} from  '@angular/http';
+import {ApiService} from './api.service'
+import 'rxjs/Rx'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'app works!';
+constructor(private ser:ApiService){}
+ngOnInit() {
+        this.ser.prueba().subscribe()
+    }
 }
